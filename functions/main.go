c5d3eff3f36f1db6a1aae445f61c59cd52f0842c
Subhayan Bhattacharya <subhayanbhattacharya@subhayans-mbp.speedport.ip>
2025-03-09 21:33:54 +0100

    add code for function which split strings and finds out how many times a word appears

--- functions/main.go
+++ functions/main.go
@@ -1,21 +1,47 @@
-// function which gives even values from a slice of numbers
+// function which takes a slice of strings and gives back how many times they appear
+// added code to also break a string into word slices
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func produceEven(values []int) []int {
-	results := []int{}
-	for _, value := range values {
-		if value%2 == 0 {
-			results = append(results, value)
+func breakAStringIntoWords(value string) []string {
+	results := []string{}
+	temp := ""
+	for _, character := range value {
+		if character == ' ' {
+			results = append(results, temp)
+			temp = ""
+
+		} else {
+			temp += string(character)
 		}
+	}
+	if len(temp) > 0 {
+		results = append(results, temp)
+	}
+	return results
+}
+
+func findHowManytimes(values ...string) map[string]int {
+	results := map[string]int{}
+	for _, value := range values {
+		_, ok := results[value]
+		if ok {
+			results[value] += 1
+		} else {
+			results[value] = 1
+		}
+
 	}
 	return results
 }
 
 func main() {
-	values := []int{20, 54, 67, 11, 3, 10}
-	check := produceEven(values)
-	fmt.Println(check)
+	sentence := "Subhayan is a great guy. Subhayan has a brother callewd Shaayan . Subhayan also has a wife"
+	broken := breakAStringIntoWords(sentence)
+	final := findHowManytimes(broken...)
+	fmt.Println(final)
 }
