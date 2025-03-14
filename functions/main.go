8ac770953a98a0318265a022f049703d60cbea68
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-03-14 19:28:12 +0100

    function taking struct as params

--- functions/main.go
+++ functions/main.go
@@ -1,5 +1,4 @@
-// function which takes a slice of strings and gives back how many times they appear
-// added code to also break a string into word slices
+// function which accepts a struct as argument and then calculates the area and perimeter
 
 package main
 
@@ -7,41 +6,22 @@ import (
 	"fmt"
 )
 
-func breakAStringIntoWords(value string) []string {
-	results := []string{}
-	temp := ""
-	for _, character := range value {
-		if character == ' ' {
-			results = append(results, temp)
-			temp = ""
-
-		} else {
-			temp += string(character)
-		}
-	}
-	if len(temp) > 0 {
-		results = append(results, temp)
-	}
-	return results
+type Rectange struct {
+	width  int
+	height int
 }
 
-func findHowManytimes(values ...string) map[string]int {
-	results := map[string]int{}
-	for _, value := range values {
-		_, ok := results[value]
-		if ok {
-			results[value] += 1
-		} else {
-			results[value] = 1
-		}
+func findPermiter(rectange Rectange) int {
+	return 2*rectange.height + 2*rectange.width
+}
 
-	}
-	return results
+func findArea(rectangle Rectange) int {
+	return rectangle.height * rectangle.width
 }
 
 func main() {
-	sentence := "Subhayan is a great guy. Subhayan has a brother callewd Shaayan . Subhayan also has a wife"
-	broken := breakAStringIntoWords(sentence)
-	final := findHowManytimes(broken...)
-	fmt.Println(final)
+	r1 := Rectange{width: 10, height: 20}
+	fmt.Println(findPermiter(r1))
+	fmt.Println(findArea(r1))
+
 }
