fd2136228815b4190028ca8562eb38bc8c413ff9
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-03-16 22:13:20 +0530

    using slice.sort to sort a slice of structs

--- functions/main.go
+++ functions/main.go
@@ -1,27 +1,31 @@
-// function which accepts a struct as argument and then calculates the area and perimeter
-
+// demostration of how sort slice function is used to sort a slice of structs
 package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-type Rectange struct {
-	width  int
-	height int
-}
-
-func findPermiter(rectange Rectange) int {
-	return 2*rectange.height + 2*rectange.width
-}
-
-func findArea(rectangle Rectange) int {
-	return rectangle.height * rectangle.width
+type Person struct {
+	name     string
+	lastName string
+	age      int
 }
 
 func main() {
-	r1 := Rectange{width: 10, height: 20}
-	fmt.Println(findPermiter(r1))
-	fmt.Println(findArea(r1))
+	people := []Person{
+		{"Subhayan", "Bhattacharya", 41},
+		{"Shaayan", "Bhattacharya", 37},
+		{"Poulomi", "Adhikary", 37},
+	}
+	for _, person := range people {
+		fmt.Println(person.lastName)
+	}
 
+	sort.Slice(people, func(i int, j int) bool {
+		fmt.Println(i, j)
+		return people[i].age < people[j].age
+	})
+
+	fmt.Println(people)
 }
