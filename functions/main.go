c264fe906eba26e9836ff720ea42edac2aaed72e
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-03-18 15:53:20 +0530

    for a map call by value is an exception

--- functions/main.go
+++ functions/main.go
@@ -1,24 +1,32 @@
 // understanding call by value
-// when a function is called a copies of the params are made
+// for a map it can change attribute values in the function
 package main
 
 import (
 	"fmt"
 )
 
-type Person struct {
-	name     string
-	lastName string
-	age      int
+func modifyMap(person map[string]string) {
+	person["name"] = "Shaayan"
 }
 
-func modifySomething(p Person) {
-	p.lastName = "Adhikary"
+func modifySlice(numbers []int) {
+	for index, number := range numbers {
+		numbers[index] = number * number
+	}
+	numbers = append(numbers, 420)
 }
 
 func main() {
-	p := Person{name: "Subhayan", lastName: "Bhattacharya", age: 40}
-	fmt.Println(p)
-	modifySomething(p)
-	fmt.Println(p)
+	person := map[string]string{
+		"name":     "Subhayan",
+		"LastName": "Bhattacharya",
+		"age":      "40",
+	}
+	fmt.Println(person)
+	modifyMap(person)
+	fmt.Println(person)
+	numbers := []int{2, 4, 6, 8}
+	modifySlice(numbers)
+	fmt.Println(numbers)
 }
