a37345309c2ebba1fd2275f4620cfe85f1712599
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-04-03 13:46:09 +0530

    Example of a switch case statement

--- functions/main.go
+++ functions/main.go
@@ -3,23 +3,16 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name     string
-	lastName string
-	age      int
-}
-
 func main() {
-	var x interface{} = Person{
-		name:     "Subhayan",
-		lastName: "Bhattacharya",
-		age:      40,
-	}
-	x2 := x
-	value, ok := x2.(int)
-	if ok {
-		fmt.Println("The value is :", value)
-	} else {
-		fmt.Println("type assertion failed.")
+	names := []string{"Subhayan", "Shaayan", "Dimpu", "Papa", "Mama"}
+	for _, name := range names {
+		switch name {
+		case "Subhayan", "Dimpu":
+			fmt.Println("Couples..")
+		case "Shaayan":
+			fmt.Println("Brother")
+		case "Papa", "Mama":
+			fmt.Println("Parents")
+		}
 	}
 }
