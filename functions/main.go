a333b4e073370122e20a9c94f4b03d956f3247ab
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-04-03 13:15:03 +0530

    type assertion in go

--- functions/main.go
+++ functions/main.go
@@ -1,36 +1,20 @@
-// compare interfaces
+// type assertion in Go
 package main
 
 import "fmt"
 
-type Doubler interface {
-	Double()
-}
-
-type DoubleInt int
-
-func (d *DoubleInt) Double() {
-	*d = *d * 2
-}
-
-type DoubleIntSlice []int
-
-func (d DoubleIntSlice) Double() {
-	for i, value := range d {
-		d[i] = value * 2
-	}
-}
-
-func DoubleComparer(d1 Doubler, d2 Doubler) {
-	fmt.Println(d1 == d2)
+type Person struct {
+	name     string
+	lastName string
+	age      int
 }
 
 func main() {
-	var i1 DoubleInt = 10
-	var i2 DoubleInt = 10
-	var slice1 = DoubleIntSlice{1, 2, 3}
-	var slice2 = DoubleIntSlice{1, 2, 3}
-	DoubleComparer(&i1, &i2)
-	fmt.Println(slice1, slice2)
-	DoubleComparer(slice1, slice2)
+	var x interface{} = Person{
+		name:     "Subhayan",
+		lastName: "Bhattacharya",
+		age:      40,
+	}
+	x2 := x
+	fmt.Println(x2.(Person))
 }
