2409bd99e27840c7cf3f9ddcda918838ae3e053b
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-05-27 17:00:14 +0200

    comparing interfaces

--- functions/main.go
+++ functions/main.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
-// We can modify the values of a map just by passing it to a function
-func changeMapValues(m map[string]int) {
-	for key, value := range m {
-		fmt.Println("Changing the value of key ", key)
-		m[key] = value * 100
+type Doubler interface {
+	Double()
+}
+
+type DoubleInt int
+
+// The method is called Double
+// And this is a method on the DoubleInt type
+func (d *DoubleInt) Double() {
+	*d = *d * 2
+}
+
+type DoubleIntSlice []int
+
+// for this we use a value receiver method
+func (d DoubleIntSlice) Double() {
+	for index, value := range d {
+		d[index] = value * 2
 	}
 }
 
+// interfaces are also comparable
+// both their values and types need to be equal
+func CompareDouble(d1, d2 Doubler) bool {
+	return d1 == d2
+}
+
 func main() {
-	m := map[string]int{
-		"Subhayan": 40,
-		"Shaayan":  37,
-		"Dimpu":    36,
-	}
-	changeMapValues(m)
-	for _, value := range m {
-		fmt.Println(value)
+	d1 := DoubleInt(10)
+	d2 := DoubleInt(10)
+	if CompareDouble(&d1, &d2) {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not comparable")
 	}
 }
