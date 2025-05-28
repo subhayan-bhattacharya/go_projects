5f8fafc298a9c5932a32f3d39ac6ff19d8943ae1
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-05-28 17:23:12 +0200

    self implementation of map and filter in go

--- functions/main.go
+++ functions/main.go
@@ -1,40 +1,42 @@
+// Self implementation of map and filter func in go using generics
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-type Doubler interface {
-	Double()
-}
-
-type DoubleInt int
-
-// The method is called Double
-// And this is a method on the DoubleInt type
-func (d *DoubleInt) Double() {
-	*d = *d * 2
-}
-
-type DoubleIntSlice []int
-
-// for this we use a value receiver method
-func (d DoubleIntSlice) Double() {
-	for index, value := range d {
-		d[index] = value * 2
+func mapDemo[T any](values []T, mapFunc func(T) T) {
+	for index, value := range values {
+		fmt.Println("Checking the value :", value)
+		values[index] = mapFunc(value)
 	}
 }
 
-// interfaces are also comparable
-// both their values and types need to be equal
-func CompareDouble(d1, d2 Doubler) bool {
-	return d1 == d2
+func filterDemo[T any](values []T, filterFunc func(T) bool) []T {
+	results := []T{}
+	for _, value := range values {
+		fmt.Println("Checking the value :", value)
+		if filterFunc(value) {
+			results = append(results, value)
+		}
+	}
+	return results
+}
+
+func upperCaseString(value string) string {
+	return strings.ToUpper(value)
+}
+
+func stringsWithMoreThan5Chars(value string) bool {
+	return len(value) > 5
 }
 
 func main() {
-	d1 := DoubleInt(10)
-	d2 := DoubleInt(10)
-	if CompareDouble(&d1, &d2) {
-		fmt.Println("Equal")
-	} else {
-		fmt.Println("Not comparable")
-	}
+	names := []string{"Subhayan", "Shaayan", "Dimpu", "Papa"}
+	mapDemo(names, upperCaseString)
+	fmt.Println(names)
+	filteredNames := filterDemo(names, stringsWithMoreThan5Chars)
+	fmt.Println(filteredNames)
 }
