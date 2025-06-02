927d690cee056acbd04f76f682b1918dc6d7be67
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-06-02 16:57:12 +0200

    implement a queue in generics in go

--- functions/main.go
+++ functions/main.go
@@ -1,42 +1,62 @@
-// Self implementation of map and filter func in go using generics
+// Implementation of a queue
 
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strings"
 )
 
-func mapDemo[T any](values []T, mapFunc func(T) T) {
-	for index, value := range values {
-		fmt.Println("Checking the value :", value)
-		values[index] = mapFunc(value)
-	}
+type Queue[T comparable] struct {
+	items []T
 }
 
-func filterDemo[T any](values []T, filterFunc func(T) bool) []T {
-	results := []T{}
-	for _, value := range values {
-		fmt.Println("Checking the value :", value)
-		if filterFunc(value) {
-			results = append(results, value)
+func (queue *Queue[T]) add(item T) {
+	queue.items = append(queue.items, item)
+}
+
+func (queue *Queue[T]) remove() (T, bool) {
+	if len(queue.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	item := queue.items[0]
+	queue.items = queue.items[1:]
+	return item, true
+}
+
+func (queue *Queue[T]) find(value T) (T, error) {
+	var zero T
+	if len(queue.items) == 0 {
+		return zero, errors.New("Queue is empty")
+	}
+	for _, item := range queue.items {
+		if item == value {
+			return value, nil
 		}
 	}
-	return results
-}
-
-func upperCaseString(value string) string {
-	return strings.ToUpper(value)
-}
-
-func stringsWithMoreThan5Chars(value string) bool {
-	return len(value) > 5
+	return zero, nil
 }
 
 func main() {
-	names := []string{"Subhayan", "Shaayan", "Dimpu", "Papa"}
-	mapDemo(names, upperCaseString)
-	fmt.Println(names)
-	filteredNames := filterDemo(names, stringsWithMoreThan5Chars)
-	fmt.Println(filteredNames)
+	var queue Queue[int]
+	queue.add(1)
+	queue.add(34)
+	queue.add(90)
+	queue.add(111)
+	value, ok := queue.find(420)
+	if ok != nil {
+		fmt.Println("Value is there : ", value)
+	} else {
+		fmt.Println("Value is not there")
+	}
+	for {
+		value, isEmpty := queue.remove()
+		if isEmpty {
+			fmt.Println(value)
+		} else {
+			fmt.Println("The queue is empty")
+			break
+		}
+	}
 }
