c8593cab94e489398a6291a74e21124f76a75cca
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-06-05 13:34:17 +0200

    using interfaces and struct types with generics

--- functions/main.go
+++ functions/main.go
@@ -3,60 +3,72 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"math"
 )
 
-type Queue[T comparable] struct {
-	items []T
+type Pair[T fmt.Stringer] struct {
+	Val1 T
+	Val2 T
 }
 
-func (queue *Queue[T]) add(item T) {
-	queue.items = append(queue.items, item)
+type Differ[T any] interface {
+	fmt.Stringer
+	Diff(T) float64
 }
 
-func (queue *Queue[T]) remove() (T, bool) {
-	if len(queue.items) == 0 {
-		var zero T
-		return zero, false
+func FindCloser[T Differ[T]](pair1 Pair[T], pair2 Pair[T]) Pair[T] {
+	d1 := pair1.Val1.Diff(pair1.Val2)
+	d2 := pair1.Val2.Diff(pair2.Val2)
+	if d1 < d2 {
+		return pair1
+	} else {
+		return pair2
 	}
-	item := queue.items[0]
-	queue.items = queue.items[1:]
-	return item, true
 }
 
-func (queue *Queue[T]) find(value T) (T, error) {
-	var zero T
-	if len(queue.items) == 0 {
-		return zero, errors.New("Queue is empty")
-	}
-	for _, item := range queue.items {
-		if item == value {
-			return value, nil
-		}
-	}
-	return zero, nil
+type Point struct {
+	x, y int
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("{%d, %d}", p.x, p.y)
+}
+
+func (p Point) Diff(p2 Point) float64 {
+	x := p.x - p2.x
+	y := p.y - p2.y
+	// formula which uses Pythagorus theorm to
+	// calculate distance between two points
+	return math.Sqrt(float64(x*x) + float64(y*y))
 }
 
 func main() {
-	var queue Queue[int]
-	queue.add(1)
-	queue.add(34)
-	queue.add(90)
-	queue.add(111)
-	value, ok := queue.find(420)
-	if ok != nil {
-		fmt.Println("Value is there : ", value)
-	} else {
-		fmt.Println("Value is not there")
+	// each of these points satisfy the interface Differ
+	p1 := Point{
+		x: 10,
+		y: 20,
 	}
-	for {
-		value, isEmpty := queue.remove()
-		if isEmpty {
-			fmt.Println(value)
-		} else {
-			fmt.Println("The queue is empty")
-			break
-		}
+	p2 := Point{
+		x: 30,
+		y: 40,
 	}
+	p3 := Point{
+		x: 5,
+		y: 15,
+	}
+	p4 := Point{
+		x: 11,
+		y: 21,
+	}
+	pair1 := Pair[Point]{
+		Val1: p1,
+		Val2: p2,
+	}
+	pair2 := Pair[Point]{
+		Val1: p3,
+		Val2: p4,
+	}
+
+	fmt.Println(FindCloser(pair1, pair2))
 }
