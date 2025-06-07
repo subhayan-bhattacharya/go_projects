775f9de33ce963b732de46f26b626cde06fbc203
Subhayan Bhattacharya <subhayan.aws@gmail.com>
2025-06-07 15:28:10 +0200

    marriage between generic functions and generic datastructures

--- functions/go.mod
+++ functions/go.mod
@@ -1,3 +1,5 @@
 module project
 
 go 1.23.5
+
+require golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
--- functions/main.go
+++ functions/main.go
@@ -1,30 +1,81 @@
-// combine type terms into interfaces example
+// The marriage between generic data structures and generic functions
+// generic Stack and functions to operate on that stack
+// We need to use an external Go package here to make sure that we can compare
+// two elements of a stack. The default comparable only does equal and not equal comparison
 
 package main
 
 import (
 	"fmt"
+
+	"golang.org/x/exp/constraints"
 )
 
-type PrintableInt interface {
-	~int
-	String() string
+type Stack[T constraints.Ordered] struct {
+	values []T
 }
 
-type newType int
-
-func (n newType) String() string {
-	return fmt.Sprintf("Value : {%d}", n)
+func (s *Stack[T]) Push(value T) {
+	s.values = append(s.values, value)
 }
 
-func PrintTwice[T PrintableInt](value T) {
-	check := value.String()
-	fmt.Println(value.String())
-	fmt.Println(check)
-	fmt.Println(value + 100)
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.values) == 0 {
+		var zero T
+		return zero, false
+	}
+	element := s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
+	return element, true
+}
+
+func (s *Stack[T]) IsThere(element T) bool {
+	for _, value := range s.values {
+		if value == element {
+			return true
+		}
+	}
+	return false
+}
+
+func FindMaxInStack[T constraints.Ordered](s Stack[T]) (T, bool) {
+	if len(s.values) == 0 {
+		var zero T
+		return zero, false
+	}
+	maxValue := s.values[0]
+	for _, value := range s.values {
+		if value > maxValue {
+			maxValue = value
+		}
+	}
+	fmt.Println("The max value about to be returned is :", maxValue)
+	return maxValue, true
 }
 
 func main() {
-	var x newType = 420
-	PrintTwice(x)
+	s := Stack[int]{}
+	fmt.Println(s)
+	s.Push(1)
+	s.Push(56)
+	s.Push(78)
+	s.Push(81)
+	fmt.Println(s)
+	for {
+		value, isNotEmpty := s.Pop()
+		if isNotEmpty {
+			fmt.Println(value)
+		} else {
+			fmt.Println("Is empty")
+			break
+		}
+
+	}
+	maxValue, isEmpty := FindMaxInStack(s)
+	if isEmpty {
+		fmt.Println("The max value in the stack is :", maxValue)
+	} else {
+		fmt.Println("The stack is empty!!")
+	}
+
 }
