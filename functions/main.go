74fdb9d66d762a31dd7f0f0155f46f4d0067dd9a
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-06-11 12:27:30 +0200

    a simple add to function to practice using variadic arguments to a function

--- functions/main.go
+++ functions/main.go
@@ -4,72 +4,17 @@
 
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"golang.org/x/exp/constraints"
-)
-
-type MapStore[K comparable, V any] map[K]V
-
-func (m MapStore[K, V]) Get(k K) (V, bool) {
-	value, ok := m[k]
-	if ok {
-		return value, true
-	} else {
-		var zero V
-		return zero, false
+func addTo(base int, values ...int) []int {
+	results := []int{}
+	for _, value := range values {
+		results = append(results, base+value)
 	}
-}
-
-func (m MapStore[K, V]) Set(k K, v V) {
-	m[k] = v
-}
-
-func (m MapStore[K, V]) Keys() []K {
-	mapLength := len(m)
-	fmt.Println(mapLength)
-	keys := []K{}
-	for key, value := range m {
-		fmt.Println("Getting value: ", value)
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-func (m MapStore[K, V]) Values() []V {
-	values := []V{}
-	for _, value := range m {
-		values = append(values, value)
-	}
-	return values
-}
-func FindKeysWithMaxValue[K comparable, V constraints.Ordered](m MapStore[K, V]) []K {
-	allValues := m.Values()
-	maxValue := allValues[0]
-	for _, value := range allValues {
-		if value > maxValue {
-			maxValue = value
-		}
-	}
-	keysWithMaxValue := []K{}
-	for key, value := range m {
-		if value == maxValue {
-			keysWithMaxValue = append(keysWithMaxValue, key)
-		}
-	}
-	return keysWithMaxValue
+	return results
 }
 
 func main() {
-	NewMapStore := MapStore[string, int]{
-		"Skoda":    1,
-		"Mercedes": 3,
-		"Porsche":  1,
-		"Hyundai":  1,
-	}
-	fmt.Println("getting key Merecedes: ", NewMapStore["Mercedes"])
-	fmt.Println(NewMapStore.Keys())
-	NewMapStore.Set("Volkswagen", 3)
-	fmt.Println("Keys which have the max value in the store is : ", FindKeysWithMaxValue(NewMapStore))
+	check := addTo(100, 23, 45, 67, 90)
+	fmt.Println(check)
 }
