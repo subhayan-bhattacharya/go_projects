324195c566f65f204799f03188ebcb8e72d88311
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-08-15 08:01:51 +0200

    replace a whole word and a substring within word

--- functions/main.go
+++ functions/main.go
@@ -1,4 +1,4 @@
-// getting the longest word in a sentence
+// replace a substring without built in replace
 
 package main
 
@@ -7,24 +7,52 @@ import (
 	"strings"
 )
 
-func getLongestWord(sentence string) string {
+func replaceWholeWord(sentence string, toReplace string, newWord string) string {
 	words := strings.Fields(sentence)
-	if len(words) == 0 {
-		return ""
-	}
-	longestWord := words[0]
-	longest := len(words[0])
-	for _, word := range words {
-		if len(word) > longest {
-			longest = len(word)
-			longestWord = word
+	for index, word := range words {
+		if word == toReplace {
+			words[index] = newWord
 		}
 	}
-	return longestWord
+	return strings.Join(words, " ")
+}
+
+func replaceSubstrings(sentence string, toReplace string, newWord string) (string, error) {
+	if len(toReplace) == 0 {
+		return "", fmt.Errorf("Invalid input passed in for substring which needs to be replaced %s", toReplace)
+	}
+	if len(sentence) == 0 {
+		return "", fmt.Errorf("The sentence is invalid %s", sentence)
+	}
+	var result strings.Builder
+	for i := 0; i < len(sentence); i++ {
+		// here in the below sentence is the trick
+		if i+len(toReplace) <= len(sentence) {
+			if sentence[i:i+len(toReplace)] == toReplace {
+				result.WriteString(newWord)
+				// below line is also very important
+				i = i + len(toReplace) - 1
+			} else {
+				result.WriteByte(sentence[i])
+			}
+		} else {
+			result.WriteByte(sentence[i])
+		}
+
+	}
+	return result.String(), nil
 }
 
 func main() {
-	sentence := "The quick brown fox"
-	longest := getLongestWord(sentence)
-	fmt.Printf("The longest word is %s", longest)
+	sentence := "Subhayan is a great guy!"
+	result := replaceWholeWord(sentence, "great", "okay")
+	fmt.Println(result)
+	new := "programming"
+	check, error := replaceSubstrings(new, "gram", "XX")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(check)
+	}
+
 }
--- functions/main_test.go
+++ functions/main_test.go
@@ -4,30 +4,119 @@ import (
 	"testing"
 )
 
-func TestLongestWord(t *testing.T) {
+func TestReplaceWord(t *testing.T) {
 	testCases := []struct {
-		name     string
-		sentence string
-		expected string
+		name      string
+		sentence  string
+		toReplace string
+		newWord   string
+		expected  string
 	}{
 		{
-			name:     "First",
-			sentence: "The quick brown fox",
-			expected: "quick",
+			name:      "First",
+			sentence:  "The quick brown fox",
+			toReplace: "brown",
+			newWord:   "white",
+			expected:  "The quick white fox",
 		},
 		{
-			name:     "Second",
-			sentence: "My name is Subhayan",
-			expected: "Subhayan",
+			name:      "Second",
+			sentence:  "My name is Subhayan",
+			toReplace: "Subhayan",
+			newWord:   "Shaayan",
+			expected:  "My name is Shaayan",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := getLongestWord(tc.sentence)
+			result := replaceWholeWord(tc.sentence, tc.toReplace, tc.newWord)
 			if result != tc.expected {
-				t.Errorf("checkAnagram failed for testcase %s, expected %s", tc.name, tc.expected)
+				t.Errorf("replace entire word failed %s, expected %s result %s", tc.name, tc.expected, result)
 			}
+		})
+	}
+}
+
+func TestReplaceSubstring(t *testing.T) {
+	testCases := []struct {
+		testName  string
+		sentence  string
+		toReplace string
+		newWord   string
+		expected  string
+		hasError  bool
+	}{
+		{
+			testName:  "First",
+			sentence:  "programming",
+			toReplace: "gram",
+			newWord:   "XX",
+			expected:  "proXXming",
+			hasError:  false,
+		},
+		{
+			testName:  "Second",
+			sentence:  "cat dog",
+			toReplace: "cat",
+			newWord:   "elephant",
+			expected:  "elephant dog",
+			hasError:  false,
+		},
+		{
+			testName:  "Third",
+			sentence:  "abcabc",
+			toReplace: "abc",
+			newWord:   "X",
+			expected:  "XX",
+			hasError:  false,
+		},
+		{
+			testName:  "Fourth",
+			sentence:  "hello world",
+			toReplace: "hello ",
+			newWord:   "",
+			expected:  "world",
+			hasError:  false,
+		},
+		{
+			testName:  "Fifth",
+			sentence:  "aaaa",
+			toReplace: "aa",
+			newWord:   "X",
+			expected:  "XX",
+			hasError:  false,
+		},
+		{
+			testName:  "Sixth",
+			sentence:  "",
+			toReplace: "aa",
+			newWord:   "X",
+			expected:  "XX",
+			hasError:  true,
+		},
+		{
+			testName:  "Seventh",
+			sentence:  "great",
+			toReplace: "",
+			newWord:   "X",
+			expected:  "grext",
+			hasError:  true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			result, err := replaceSubstrings(tc.sentence, tc.toReplace, tc.newWord)
+			if err != nil {
+				if !tc.hasError {
+					t.Error("Expected an error to be returned")
+				}
+			} else {
+				if result != tc.expected {
+					t.Errorf("replace substring failed for testcase %s, expected %s result %s", tc.testName, tc.expected, result)
+				}
+			}
+
 		})
 	}
 }
