262dfba1bfba99889b10515d2af5b16df3f9b51e
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2025-08-16 14:53:34 +0200

    find first non repeating character in a string

--- functions/main.go
+++ functions/main.go
@@ -1,58 +1,30 @@
-// replace a substring without built in replace
+// find first non repeating character in a string
 
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func replaceWholeWord(sentence string, toReplace string, newWord string) string {
-	words := strings.Fields(sentence)
-	for index, word := range words {
-		if word == toReplace {
-			words[index] = newWord
-		}
-	}
-	return strings.Join(words, " ")
-}
+func findFirstNonRepeatingCharacter(s string) string {
+	runes := []rune(s)
+	counter := make(map[rune]int, len(runes))
 
-func replaceSubstrings(sentence string, toReplace string, newWord string) (string, error) {
-	if len(toReplace) == 0 {
-		return "", fmt.Errorf("Invalid input passed in for substring which needs to be replaced %s", toReplace)
-	}
-	if len(sentence) == 0 {
-		return "", fmt.Errorf("The sentence is invalid %s", sentence)
-	}
-	var result strings.Builder
-	for i := 0; i < len(sentence); i++ {
-		// here in the below sentence is the trick
-		if i+len(toReplace) <= len(sentence) {
-			if sentence[i:i+len(toReplace)] == toReplace {
-				result.WriteString(newWord)
-				// below line is also very important
-				i = i + len(toReplace) - 1
-			} else {
-				result.WriteByte(sentence[i])
-			}
+	for _, r := range runes {
+		_, ok := counter[r]
+		if ok {
+			counter[r]++
 		} else {
-			result.WriteByte(sentence[i])
+			counter[r] = 1
 		}
-
 	}
-	return result.String(), nil
+	for _, r := range runes {
+		if counter[r] == 1 {
+			return string(r)
+		}
+	}
+	return ""
 }
 
 func main() {
-	sentence := "Subhayan is a great guy!"
-	result := replaceWholeWord(sentence, "great", "okay")
-	fmt.Println(result)
-	new := "programming"
-	check, error := replaceSubstrings(new, "gram", "XX")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println(check)
-	}
-
+	check := findFirstNonRepeatingCharacter("swiss")
+	fmt.Println(check)
 }
