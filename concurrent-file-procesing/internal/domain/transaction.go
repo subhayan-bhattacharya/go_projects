f4560e80b3fa6d76c37683beab242504a98ad31e
subhayanbhattacharya <subhayan.aws@gmail.com>
2026-03-15 16:22:18 +0100

    Add more changes to transaction and generator

--- concurrent-file-procesing/internal/domain/transaction.go
+++ concurrent-file-procesing/internal/domain/transaction.go
@@ -1,1 +1,30 @@
 package domain
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"time"
+)
+
+type Transaction struct {
+	TransactionId string
+	CustomerId    string
+	MerchantId    string
+	Amount        float64
+	Currency      string
+	TimeStamp     time.Time
+}
+
+func (t Transaction) ToCsv() (string, error) {
+	var buf bytes.Buffer
+	writer := csv.NewWriter(&buf)
+	row := []string{
+		t.TransactionId,
+		t.CustomerId,
+		t.MerchantId,
+		fmt.Sprintf("%.2f", t.Amount),
+		t.Currency,
+		t.TimeStamp.Format(time.RFC3339),
+	}
+}
--- concurrent-file-procesing/internal/generator/generator.go
+++ concurrent-file-procesing/internal/generator/generator.go
@@ -1,11 +1,46 @@
 package generator
 
-type CommandConfig struct {
-	Rows   int
-	Files  int
-	OutDir string
+import (
+	"concurrent-file-procesing/internal/domain"
+	"fmt"
+	rand "math/rand/v2"
+	"time"
+)
+
+type Generator struct {
+	rng  *rand.Rand
+	rows int
 }
 
-func (c CommandConfig) Run() error {
-	return nil
+func NewGenerator(rows int) Generator {
+	return Generator{
+		rand.New(rand.NewPCG(42, 0)),
+		rows,
+	}
+}
+
+func (g Generator) GenerateTransaction() []domain.Transaction {
+	result := make([]domain.Transaction, g.rows)
+	currencies := []string{"EUR", "GBP", "USD", "INR", "CHF"}
+	for _ = range g.rows {
+		transaction := domain.Transaction{
+			TransactionId: fmt.Sprintf("TXN-%s", GenerateRandomString(6, g.rng)),
+			CustomerId:    fmt.Sprintf("CUST-%s", GenerateRandomString(6, g.rng)),
+			MerchantId:    fmt.Sprintf("MERC-%s", GenerateRandomString(6, g.rng)),
+			Amount:        g.rng.Float64() * 1000,
+			Currency:      currencies[g.rng.IntN(len(currencies))],
+			TimeStamp:     time.Time{},
+		}
+		result = append(result, transaction)
+	}
+	return result
+}
+
+func GenerateRandomString(length int, rng *rand.Rand) string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := range length {
+		result[i] = charset[rng.IntN(len(charset))]
+	}
+	return string(result)
 }
