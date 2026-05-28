bc42b12652500a99d7d965159ab43d8dc3619f94
subhayanbhattacharya <subhayan.aws@gmail.com>
2026-05-28 23:49:13 +0200

    using handler to create an http webserver and template strings

--- goexcercises/cyoa/cmd/cyoaweb/main.go
+++ goexcercises/cyoa/cmd/cyoaweb/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"cyoa"
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -14,21 +15,17 @@ func readFile(file string) (cyoa.Story, error) {
 		fmt.Println(err)
 	}
 	defer jsonFile.Close()
-	var story cyoa.Story
-	decoder := json.NewDecoder(jsonFile)
-	if err := decoder.Decode(&story); err != nil {
-		fmt.Println(err)
-	}
-	return story, nil
+	return cyoa.JsonStory(jsonFile)
 }
 
 func main() {
+	port := flag.Int("port", 3000, "the port number in which the application runs")
 	input := flag.String("input", "data.json", "The input json to the code.")
 	story, err := readFile(*input)
 	if err != nil {
 		fmt.Println(err)
 	}
-	for key, _ := range story {
-		fmt.Println(key)
-	}
+	handler := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
--- goexcercises/cyoa/story.go
+++ goexcercises/cyoa/story.go
@@ -1,6 +1,22 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
 type Story map[string]Chapter
+
+func JsonStory(r io.Reader) (Story, error) {
+	var story Story
+	decoder := json.NewDecoder(r)
+	if err := decoder.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
 
 type Chapter struct {
 	Title      string   `json:"title"`
@@ -12,3 +28,45 @@ type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
 }
+
+type handler struct {
+	s Story
+}
+
+var tpl = template.Must(template.New("chapter").Parse(defaultHandlerTpl))
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		http.Error(w, "Could not execute template code", http.StatusInternalServerError)
+		return
+	}
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+var defaultHandlerTpl = `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Choose your own adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>
+`
