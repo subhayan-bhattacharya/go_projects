ebb3852a8bad1e58782717f4c5f12262130ea005
subhayanbhattacharya <subhayan.aws@gmail.com>
2026-06-07 01:42:50 +0200

    extend code to generate node elements.

--- goexcercises/htmlparser/parse.go
+++ goexcercises/htmlparser/parse.go
@@ -1,8 +1,8 @@
 package htmlparser
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -15,21 +15,50 @@ type Link struct {
 
 // Parse would take an HTML document in and would parse me the links from it
 func Parse(r io.Reader) ([]Link, error) {
+	var links []Link
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
-	return nil, nil
+	for _, linkNode := range linkNodes(doc) {
+		links = append(links, buildLinks(linkNode))
+	}
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
+func buildLinks(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
 	}
-	fmt.Println(padding, msg)
-	for c := range n.ChildNodes() {
-		dfs(c, padding+"  ")
+	ret.Text = text(n)
+	return ret
+}
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
 	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	var nodes []*html.Node
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		childLinkNodes := linkNodes(c)
+		nodes = append(nodes, childLinkNodes...)
+	}
+	return nodes
 }
